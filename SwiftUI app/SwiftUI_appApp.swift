58ef98fe614be09a06401465b141b90825b16a73
hkeian47 <hanakeinan11@gmail.com>
2023-02-02 11:51:02 -0600

    first commit

---
+++ SwiftUI app/SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_appApp.swift
+//  SwiftUI app
+//
+//  Created by Hana Keinan on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
