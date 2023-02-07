4f193f4212af7b704f4d18ad4ebba0061da68c06
hkeian47 <hanakeinan11@gmail.com>
2023-02-07 12:32:44 -0600

    add a frosted glass

--- SwiftUI app/ContentView.swift
+++ SwiftUI app/ContentView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(alignment: .leading, spacing: 8.0) {
+             Image("go")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0 , height: 26.0)
+            Text("SwiftUI for IOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.secondary)
+                
+                Text("Build an iOS app for iOS 15 with custom layouts animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+            }
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background(Color("background color"))
+        .cornerRadius(30.0)
+        .shadow(radius: 20)
+        .shadow(color: Color("Color"), radius: 10, x: 0, y: 10)
+        .padding(.horizontal,20)
+        
         }
-        .padding()
-    }
-}
+            
+        }
+     
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
